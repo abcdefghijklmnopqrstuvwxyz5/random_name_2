ee48cdbf1cf03184c5252729296173a097f245de
0-122-6 <0-122-6>
2020-11-25 18:34:08 +0500

    commit message

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 
 import random as rd
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
+from UI import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.painter = False
-        uic.loadUi('UI.ui', self)
+        self.setup_Ui(self)
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
@@ -25,7 +25,7 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def paint(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(rd.choice(range(256)), rd.choice(range(256)), rd.choice(range(256))))
         x, y = rd.choice(range(600)), rd.choice(range(600))
         r = rd.choice(range(min(600 - x, 600 - y)))
         qp.drawEllipse(x, y, r, r)
