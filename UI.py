761ec821870717605e57bfcb235a4b7f3a50cf6b
0-122-6 <0-122-6>
2020-11-25 18:39:41 +0500

    commit message

--- UI.py
+++ UI.py
@@ -38,4 +38,3 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "Кнопка"))
-#
--- main.py
+++ main.py
@@ -36,4 +36,3 @@ if __name__ == '__main__':
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
-    #
